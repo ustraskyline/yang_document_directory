352912b0b6ce476f2cf6bcce24be156aab2bcb93
yang <yang628425@163.com>
2017-08-27 22:17:23 +0800

    upload Struts2

---
+++ Struts2/chapter01/src/cn/itcast/action/HelloWorldAction.java
@@ -0,0 +1,9 @@
+package cn.itcast.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class HelloWorldAction extends ActionSupport {
+	public String execute() {
+		return SUCCESS;
+	}
+}
